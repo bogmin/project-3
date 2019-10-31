e62685d7b1d119c12d69cb9979d9fca0db57421c
Andrew McGuffin <andrew.m.mcguffin-1@ou.edu>
2019-10-31 06:40:48 -0700

    oh man! thats daysofanymonth

--- DateTimeTwo.java
+++ DateTimeTwo.java
@@ -43,6 +43,33 @@ public class DateTimeTwo {
 		String dayOfWeek2 = new SimpleDateFormat("EEEE", Locale.ENGLISH).format(dateIndividual2);
 		System.out.println("The tenth day of this month is " + dayOfWeek.toUpperCase() + " and eighteenth is " + dayOfWeek2.toUpperCase());
 	}
+	public void daysOfAnyMonth(int month, int year) {
+		this.month = month;
+		this.year = year;
+		YearMonth monthDate = YearMonth.of(year, month);
+		int daysInMonth = monthDate.lengthOfMonth();
+		int fifteenth = 15;
+		String output = String.format("%d-%d-%d", year, month, fifteenth);
+		Date dateIndividual = null;
+		try {
+			dateIndividual = new SimpleDateFormat("yyyy-M-d").parse(output);
+		}
+		catch (ParseException e) {
+			e.printStackTrace();
+		}
+		String output2 = String.format("%d-%d-%d", year, month, daysInMonth);
+		Date dateIndividual2 = null;
+		try {
+			dateIndividual2 = new SimpleDateFormat("yyyy-M-d").parse(output2);
+		}
+		catch (ParseException e) {
+			e.printStackTrace();
+		}
+		String dayOfWeek = new SimpleDateFormat("EEEE", Locale.ENGLISH).format(dateIndividual);
+		String dayOfMonth = new SimpleDateFormat("EEEE", Locale.ENGLISH).format(dateIndividual2);
+		
+		System.out.println("For the year (" + year + ") and month (" + month + "), the fifteenth day is " + dayOfWeek.toUpperCase() + " and the last day is " + dayOfMonth.toUpperCase());
+	}
 	
 
 
