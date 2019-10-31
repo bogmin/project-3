1f373c4ebac8f126d7fbe0af5cf4eb6a1b3eec56
Andrew McGuffin <andrew.m.mcguffin-1@ou.edu>
2019-10-31 06:50:49 -0700

    donedonedonedone

--- MesoEquivalent.java
+++ MesoEquivalent.java
@@ -13,7 +13,34 @@ public class MesoEquivalent {
 		avg = finder.calAverage();
 		this.avgToFind = avg;
 	}
+	public void readIn() throws IOException{
+		int i = 0;
+		BufferedReader reader = new BufferedReader(new FileReader("Mesonet.txt"));
+		String stations = reader.readLine();
+		stations = reader.readLine();
+		stations = reader.readLine();
+		stations = reader.readLine();
+		while(stations != null) {
+			String cut = stations.trim().substring(0,4);
+			stationNames[i] = cut;
+			i++;
+			stations = reader.readLine();
+		}
+	}
 	
+	public HashMap<String,Integer> calAsciiEqual() throws IOException {
+		readIn();
+		HashMap<String,Integer> ascii = new HashMap<String,Integer>();
+		int newAvg = 0;
+		for(int i = 0; i < stationNames.length; ++i) {
+			MesoAsciiCal newFind = new MesoAsciiCal(new MesoStation(stationNames[i]));
+			newAvg = newFind.calAverage();
+			if(newAvg == avgToFind) {
+				ascii.put(stationNames[i],newAvg);
+			}
+			}
+		return ascii;
+		}
 		}
 
 		
